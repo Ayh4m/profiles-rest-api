2255217724ec837f1d48fe67cb313ffd08fe2778
AYH4M <ayh4m@hotmail.com>
2019-10-07 18:45:11 +0300

    Custom user profile model & manager has been created successfully

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 
-# Create your models here.
+from . import managers
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = managers.UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        """Return string representation of the user object"""
+        return self.email
+
+    def get_full_name(self):
+        """Returns the first_name plus the last_name, with a space in between"""
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        """Returns the short name for the user"""
+        return self.first_name
